88d6ca012fc5070663ecf215dbb8f852151499b9
vladlen <vladlenkaraush@gmail.com>
2018-03-11 11:49:19 +0300

    Added static serving, also another mapping on /test/signin/ to test new html page.

--- src/main/java/com/whoslast/Application.java
+++ src/main/java/com/whoslast/Application.java
@@ -6,6 +6,7 @@ import com.whoslast.entities.User;
 import com.whoslast.controllers.LiveQueueRepository;
 import com.whoslast.controllers.UserRepository;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
@@ -17,10 +18,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @ComponentScan(basePackages = {"com"})
 @EntityScan(basePackages = {"com"})
 @EnableJpaAuditing
+@EnableAutoConfiguration
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 }
-
-
--- src/main/java/com/whoslast/controllers/MainController.java
+++ src/main/java/com/whoslast/controllers/MainController.java
@@ -11,31 +11,47 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @EnableJpaRepositories
-@RequestMapping(path="/test") // URL beginning
+@RequestMapping(path = "/test") // URL beginning
 public class MainController {
-	@Autowired
-	private UserRepository userRepository;
-	@Autowired
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
     private LiveQueueRepository liveQueueRepository;
 
 
-	// @ResponseBody means the returned String is the response, not a view name
-	// @RequestParam means it is a parameter from the GET or POST request
-	@GetMapping(path="/add") // Map ONLY GET Requests
-	public @ResponseBody String addNewUser (@RequestParam String name
-			, @RequestParam String email, @RequestParam String password) {
+    // @ResponseBody means the returned String is the response, not a view name
+    // @RequestParam means it is a parameter from the GET or POST request
+    @GetMapping(path = "/add") // Map ONLY GET Requests
+    public @ResponseBody
+    String addNewUser(@RequestParam String name
+            , @RequestParam String email, @RequestParam String password) {
 
-		User n = new User();
-		n.setName(name);
-		n.setEmail(email);
-		n.setPassword(password);
-		userRepository.save(n);
-		return "Saved";
-	}
-	
+        User n = new User();
+        n.setName(name);
+        n.setEmail(email);
+        n.setPassword(password);
+        userRepository.save(n);
+        return "Saved";
+    }
+	/*
 	@GetMapping(path="/all")
 	public @ResponseBody Iterable<User> getAllUsers() {
 		// returns JSON with all users
 		return userRepository.findAll();
-	}
+	}*/
+
+    @GetMapping(path = "/all")
+    public String getAllUsers() {
+        // returns JSON with all users
+        System.out.println("showing all users");
+        return "index";
+    }
+
+    @GetMapping(path = "/signin")
+    public String signIn() {
+        // returns JSON with all users
+        System.out.println("signing in");
+        return "sign_in";
+    }
+
 }
